a70a49d79eff833a7a7aeeccbd0c2baac5930750
pierre <rischpierre@gmail.com>
2022-06-28 23:13:29 -0400

    switch to cmake instead of make file

--- main.cpp
+++ main.cpp
@@ -3,11 +3,13 @@
 #include <cstdio>
 
 #include <pxr/usd/usd/stage.h>
+#include <pxr/usd/usd/prim.h>
+#include <pxr/usd/usd/attribute.h>
 
 int main(int argc, char *argv[]){
-    auto stage = pxr::UsdStage::CreateInMemory();
-//    SdfLayer layer = SdfLayer();
-    printf("test\n");
 
+    auto stage = pxr::UsdStage::Open("../exampleUSD.usda");
+    auto primitive = stage->GetPrimAtPath(pxr::SdfPath {"/Cube/Cube"});
+    auto attribute = primitive.GetAttribute(pxr::TfToken {"points"});
     return 0;
 }

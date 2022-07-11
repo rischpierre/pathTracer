59b7612392c4e68334801f0f1dc671077a75ecc4
pierre <rischpierre@gmail.com>
2022-07-11 19:04:59 -0400

    start getting attributes from usd

--- src/scene.cpp
+++ src/scene.cpp
@@ -4,11 +4,32 @@
 Scene::Scene(const std::string &path){
 
     // todo get info out of usd scene
-//    auto stage = pxr::UsdStage::Open(path);
-//    auto primitive = stage->GetPrimAtPath(pxr::SdfPath {"/Cube/Cube"});
-//    auto attribute = primitive.GetAttribute(pxr::TfToken {"points"});
-//    Scene::points = new pxr::VtVec3fArray();
-//    attribute.Get(Scene::points, 0);
+    auto stage = pxr::UsdStage::Open(path);
+    auto primitive = stage->GetPrimAtPath(pxr::SdfPath {"/Cube/Cube"});
+    auto pointsAttr = primitive.GetAttribute(pxr::TfToken {"pointsVt"});
+    auto faceVertexIndicesAttr = primitive.GetAttribute(pxr::TfToken {"faceVertexIndices"});
+    auto normalsAttr = primitive.GetAttribute(pxr::TfToken {"normals"});
+
+    auto *normalsVt = new pxr::VtVec3fArray;
+    normalsAttr.Get(normalsVt, 0);
+
+//    cout << normalsVt[0] << endl;
+
+    auto *pointsVt = new pxr::VtVec3fArray;
+    pointsAttr.Get(pointsVt, 0);
+
+    auto *faceVertexIndices = new pxr::VtIntArray;
+    faceVertexIndicesAttr.Get(faceVertexIndices, 0);
+
+    Vector3f points2[pointsVt->size()];
+    int i = 0;
+    for(auto &p: *pointsVt){
+        points2[i] = Vector3f(p[0], p[1], p[2]);
+        i++;
+    }
+    cout << points2[0][0] << endl;
+
+
     Vector3f v1(1, 10, -2);
     Vector3f v2(1, 10, 1);
     Vector3f v3(-2, 10, 1);
